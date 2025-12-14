7728a80b0057f3ee2d40272ef255f42e75dac84e
that-damn-cat <saihenjin@gmail.com>
2025-12-14 11:57:38 -0600

    drain only affects pickups

--- Entities/Drain/drain.gd
+++ Entities/Drain/drain.gd
@@ -1,17 +1,18 @@
 extends Marker2D
 
-@export var drain_gravity = 1400.0
+@export var drain_gravity = 2500.0
 var velocity_bodies: Array[CharacterBody2D]
 
 func _ready() -> void:
-	pass
-	# await globals.data_ready
+	await globals.data_ready
 
-	# globals.player_container.child_entered_tree.connect(add_affected_child)
-	# globals.enemy_container.child_entered_tree.connect(add_affected_child)
+	#globals.player_container.child_entered_tree.connect(add_affected_child)
+	#globals.enemy_container.child_entered_tree.connect(add_affected_child)
+	globals.pickup_container.child_entered_tree.connect(add_affected_child)
 
-	# globals.player_container.child_exiting_tree.connect(remove_affected_child)
-	# globals.enemy_container.child_exiting_tree.connect(remove_affected_child)
+	#globals.player_container.child_exiting_tree.connect(remove_affected_child)
+	#globals.enemy_container.child_exiting_tree.connect(remove_affected_child)
+	globals.pickup_container.child_exiting_tree.connect(add_affected_child)
 
 	# for child in globals.player_container.get_children():
 	# 	add_affected_child(child)
@@ -19,18 +20,25 @@ func _ready() -> void:
 	# for child in globals.enemy_container.get_children():
 	# 	add_affected_child(child)
 
+	for child in globals.pickup_container.get_children():
+		add_affected_child(child)
+
 func _physics_process(_delta: float) -> void:
-	pass
-	# for body in velocity_bodies:
-	# 	var gravity_direction: Vector2 = body.global_position.direction_to(global_position)
+	for body in velocity_bodies:
+		if not is_instance_valid(body):
+			continue
 
-	# 	var body_mass = body.get("mass")
-	# 	if not body_mass:
-	# 		body_mass = 16.0
+		var gravity_direction: Vector2 = body.global_position.direction_to(global_position)
 
-	# 	var radius_squared = pow(global_position.distance_to(body.global_position), 2)
+		var body_mass = body.get("mass")
+		if not body_mass:
+			body_mass = 16.0
 
-	# 	body.velocity += gravity_direction * ((drain_gravity * body_mass) / radius_squared)
+		var radius_squared = pow(global_position.distance_to(body.global_position), 2)
+
+		body.velocity += gravity_direction * ((drain_gravity * body_mass) / radius_squared)
+
+	clean_array(velocity_bodies)
 
 func add_affected_child(node: Node) -> void:
 	if not node is CharacterBody2D:
@@ -53,3 +61,14 @@ func remove_affected_child(node: Node) -> void:
 		return
 
 	velocity_bodies.erase(node)
+
+func clean_array(arr: Array) -> void:
+	var to_remove: Array[int] = []
+
+	for index in range(arr.size()):
+		if arr[index] == null:
+			to_remove.append(index)
+
+	for index in to_remove:
+		if index < arr.size():
+			arr.remove_at(index)
