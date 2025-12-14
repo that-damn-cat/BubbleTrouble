91d260c00a021214c0c826c09807452cdb436b7e
that-damn-cat <saihenjin@gmail.com>
2025-12-14 11:58:20 -0600

    Extended merge and split logic

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -20,6 +20,7 @@ extends CharacterBody2D
 @export var min_speed: float = 175.0
 @export var max_speed: float = 550.0
 @export_range(0.0, 1.0, 0.01) var friction = .09
+@export var bounce_damping: float = 0.6
 
 var direction := Vector2.ZERO
 
@@ -40,11 +41,22 @@ func update_mass() -> void:
 	%MassLabel.text = "Mass: %0.1f" % mass
 
 func merge(consumed: Player) -> void:
+	var initial_health: float = health_component.current_health
+
 	health_component.heal(consumed.mass)
 	update_mass()
-	consumed.queue_free()
 
-func _physics_process(_delta):
+	var healed_amount: float = health_component.current_health - initial_health
+
+	if healed_amount == consumed.mass:
+		consumed.queue_free()
+		return
+
+	consumed.mass = consumed.mass - healed_amount
+	consumed.update_mass()
+
+
+func _physics_process(delta):
 	var acceleration = remap(mass, min_mass, max_mass, max_acceleration, min_acceleration)
 	var speed_cap = remap(mass, min_mass, max_mass, max_speed, min_speed)
 
@@ -56,22 +68,34 @@ func _physics_process(_delta):
 
 	move_and_slide()
 
-func _on_merge_area_body_entered(body: Node2D) -> void:
-	if body is not Player:
-		return
 
+func _on_merge_area_body_entered(body: Node2D) -> void:
 	if body == self:
 		return
 
-	body = body as Player
-
-	if mass < body.mass:
+	if health_component.current_health == health_component.max_health:
+		if body is Droplet:
+			%Active.try_split()
 		return
 
-	merge(body)
+	if body is Player:
+		if mass < body.mass:
+			return
+
+		merge(body)
+		return
+
+	if body is Droplet:
+		health_component.heal(body.size_bonus)
+		update_mass()
+		body.queue_free()
 
 func _on_healed(_amount: float) -> void:
+	#if health_component.current_health == health_component.max_health:
+	#	%MergeCollider.set_deferred("disabled", true)
+
 	update_mass()
 
 func _on_damaged(_amount: float) -> void:
+	#%MergeCollider.set_deferred("disabled", false)
 	update_mass()
